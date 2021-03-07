073ae11a7960dd95a1c455a5866c1e35ce1aa8a7
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2021-03-07 22:42:01 +0530

    Controllers 3 and Views : CRUD operations

--- EmployeeSystem/Controllers/EmployeeSystemController.cs
+++ EmployeeSystem/Controllers/EmployeeSystemController.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,84 @@ namespace EmployeeSystem.Controllers
                                     select e).ToList();
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            FillCountries();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Insert(Employee model)
+        {
+            FillCountries();
+
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(model);
+                db.SaveChanges();
+                ViewBag.Message = "Employee added successfully";
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            FillCountries();
+            var model = db.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult update(Employee model)
+        {
+            FillCountries();
+
+            if (ModelState.IsValid)
+            {
+                db.Employees.Update(model);
+                db.SaveChanges();
+                ViewBag.Message = "Employee updated successfully";
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var model = db.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var model = db.Employees.Find(id);
+            db.Employees.Remove(model);
+            db.SaveChanges();
+            TempData["Message"] = "Employee Deleted Successfully.";
+            return RedirectToAction("List");
+        }
+
+        private void FillCountries()
+        {
+            var listOfCountries = (from c in db.Employees
+                                   select new SelectListItem()
+                                   {
+                                       Text = c.Country,
+                                       Value = c.Country
+                                   }).Distinct().ToList();
+
+            ViewBag.Countries = listOfCountries;
         }
     }
 }
