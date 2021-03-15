bd59bf07fcc56f18b7995b2aaf921b2c39b49860
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2021-03-15 22:59:25 +0530

    Identity Server - SignIn & Register 2

--- EmployeeSystem/Models/Register.cs
+++ EmployeeSystem/Models/Register.cs
@@ -1,7 +1,34 @@
-﻿namespace EmployeeSystem.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeSystem.Models
 {
     public class Register
     {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [Display(Name = "Confirm Password")]
+        public string ComfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
     }
 }
